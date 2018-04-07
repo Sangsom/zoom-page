64a27638500b652a52208c63fdccada7828f42c5
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-07 13:03:55 +0300

    first commit

---
+++ src/components/Root.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Root extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>zoom page</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+          repellendus deleniti necessitatibus, magnam vel corrupti impedit
+          accusantium consequuntur, minima quis aliquam doloremque officiis
+          similique quam culpa, alias dicta ipsa doloribus.
+        </p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Root;
