702cf6e43e4d6d511bcd3b2278a0e8276a6f31b2
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-09 15:10:52 +0300

    feat: Fully Responsive section

---
+++ src/components/FullyResponsive.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const FullyResponsive = () => {
+  return (
+    <section id="fullyResponsive">
+      <div className="left">
+        <div className="phone leftPhone">
+          <img src="./img/left-phone.png" alt="Left Phone" />
+        </div>
+        <div className="phone rightPhone">
+          <img src="./img/right-phone.png" alt="Right Phone" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Fully Responsive</h2>
+          <p>
+            Eum cu tantas legere complectitur, hinc utamur ea eam. Lorem ipsum
+            dolor sit amet, volumus temporibus definitionem id ius, vel ut magna
+            timeam appareat, movet timeam maiorum an ius. Sea libris dictas
+            lucilius et. Ne cibo urbanitas pri, qui ad populo accusamus. Quod
+            congue ea ius, nisl aperiri sit ut.
+          </p>
+          <a href="" className="actionButton">
+            Order me one
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FullyResponsive;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Services from "./Services";
 import WebPages from "./WebPages";
+import FullyResponsive from "./FullyResponsive";
 
 const Main = () => {
   return (
     <main>
       <Services />
       <WebPages />
+      <FullyResponsive />
     </main>
   );
 };
