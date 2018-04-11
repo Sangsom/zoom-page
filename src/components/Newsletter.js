6241bb0efce1d36ca09e84872da8c250b8e4114e
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-11 16:21:11 +0300

    Seperating Newsletter stylesheets

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/Newsletter.css";
 
 const Newsletter = () => {
   return (
-    <section id="newsletter">
+    <section id="newsletter" className="Newsletter">
       <h2>Want to hear more story, subscribe for our newsletter</h2>
-      <a href="" className="subscribe">
+      <a href="" className="Newsletter_subscribe">
         Subscribe
       </a>
     </section>
