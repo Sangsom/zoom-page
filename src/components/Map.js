74f44f1b59627c5705f4c3a3f9aa335a54c0b535
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-11 16:41:06 +0300

    Seperating Map stylesheets

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../styles/Map.css";
 
 const Map = () => {
   return (
-    <div id="mapSection">
-      <div className="left" />
-      <div className="right">
-        <div className="address">
+    <div id="mapSection" className="Map">
+      <div className="Map_left" />
+      <div className="Map_right">
+        <div className="Map_address">
           <p>Awesome Address 432</p>
           <p>ourmail@sometype.com</p>
           <p>+321(0)32 123 343 492</p>
