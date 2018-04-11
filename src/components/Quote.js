2139cbe9c7f6a4615ebcdff4738d302cb8191d53
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-11 16:19:34 +0300

    Seperating Quote stylesheets

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,14 +1,23 @@
 import React from "react";
+import "../styles/Quote.css";
 
 const Quote = () => {
   return (
-    <section id="quote">
+    <section id="quote" className="Quote">
       <img src="./img/quote.png" alt="Quote" />
       <h2>We realize and actualize your vision</h2>
       <h3>John Doe CEO</h3>
-      <div className="arrows">
-        <img src="./img/left-arrow.png" className="arrow" alt="Left arrow" />
-        <img src="./img/right-arrow.png" className="arrow" alt="Right arrow" />
+      <div className="Quote_arrows">
+        <img
+          src="./img/left-arrow.png"
+          className="Quote_arrow"
+          alt="Left arrow"
+        />
+        <img
+          src="./img/right-arrow.png"
+          className="Quote_arrow"
+          alt="Right arrow"
+        />
       </div>
     </section>
   );
