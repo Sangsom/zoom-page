aeeea505851f91387a50d093d00732420e73ea33
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-12 10:09:44 +0300

    feat: Setting mediaquery for navigation

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
+import MediaQuery from "react-responsive";
 import "../styles/Navigation.css";
 
 const Navigation = () => {
   return (
-    <nav className="Navigation">
-      <ul>
-        <li className="active">
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="#serviceSection">Services</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#contactUs">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <Fragment>
+      <MediaQuery minDeviceWidth={1024}>
+        <nav className="Navigation">
+          <ul>
+            <li className="active">
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#serviceSection">Services</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contactUs">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={1023}>
+        <p>Hamburegr</p>
+      </MediaQuery>
+    </Fragment>
   );
 };
 
