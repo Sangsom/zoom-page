03b72a6eb9cdd976dc3ad4570c78b1eb49349bd0
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-12 16:14:22 +0300

    style: FullyResponsive responsive

--- src/components/FullyResponsive.js
+++ src/components/FullyResponsive.js
@@ -4,14 +4,15 @@ import "../styles/FullyResponsive.css";
 const FullyResponsive = () => {
   return (
     <section id="fullyResponsive" className="FullyResponsive">
-      <div className="FullyResponsive_left">
-        <div className="FullyResponsive_phone FullyResponsive_leftPhone">
+      <div className="FullyResponsive_phoneWrapper">
+        <div className="FullyResponsive_leftPhone">
           <img src="./img/left-phone.png" alt="Left Phone" />
         </div>
-        <div className="FullyResponsive_phone FullyResponsive_rightPhone">
+        <div className="FullyResponsive_rightPhone">
           <img src="./img/right-phone.png" alt="Right Phone" />
         </div>
       </div>
+
       <div className="FullyResponsive_right">
         <div className="FullyResponsive_wrapper">
           <h2>Fully Responsive</h2>
