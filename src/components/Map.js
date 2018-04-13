e1e0f646a1d4b8a3adee627f9075ac8d7ecd789b
Rinalds Pudulis <rpudulis@gmail.com>
2018-04-13 15:47:50 +0300

    feat: Added Google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "../styles/Map.css";
 
-const Map = () => {
-  return (
-    <div id="mapSection" className="Map">
-      <div className="Map_left" />
-      <div className="Map_right">
-        <div className="Map_address">
-          <p>Awesome Address 432</p>
-          <p>ourmail@sometype.com</p>
-          <p>+321(0)32 123 343 492</p>
+class Map extends React.Component {
+  componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: 56.949649, lng: 24.105186 },
+      zoom: 11,
+      mapTypeId: "roadmap"
+    });
+  }
+
+  render() {
+    return (
+      <div id="mapSection" className="Map">
+        <div id="map" />
+        <div className="Map_right">
+          <div className="Map_address">
+            <p>Awesome Address 432</p>
+            <p>ourmail@sometype.com</p>
+            <p>+321(0)32 123 343 492</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Map;
